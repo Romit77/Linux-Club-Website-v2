749e5a866eda9b3a22dd14b7e7d78781a0fbf84f
Romit  Dey <romit77dey@gmail.com>
2024-04-22 20:49:06 +0530

    join us page

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 
-export default function BackgroundBeamsDemo() {
+export default function Contact() {
   return (
     <div className="h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
