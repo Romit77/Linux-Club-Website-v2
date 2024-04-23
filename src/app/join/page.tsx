a368b04dcec314feb169f8ab686223fc27588ea1
Romit  Dey <romit77dey@gmail.com>
2024-04-23 16:55:03 +0530

    navbar and event page changes

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,18 +1,23 @@
 "use client";
-
 import React from "react";
 import { motion } from "framer-motion";
+import { Navbar } from "@/components/component/Navbar";
+import { Footer } from "@/components/component/footer";
 
 function page() {
   return (
-    <motion.div
-      className="text-white flex items-center justify-center h-screen text-4xl font-bold overflow-hidden"
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.4 }}
-    >
-      Recruitmment Closed for now :)
-    </motion.div>
+    <>
+      <Navbar />
+      <motion.div
+        className="text-white flex items-center justify-center h-screen md:text-4xl  font-bold overflow-hidden"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.4 }}
+      >
+        Recruitmment Closed for now :)
+      </motion.div>
+      <Footer />
+    </>
   );
 }
 
