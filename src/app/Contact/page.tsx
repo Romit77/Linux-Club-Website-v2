0b2bf393ddc89a8b50ba31c07d7ebd43eec905ce
Romit  Dey <romit77dey@gmail.com>
2024-04-30 00:20:37 +0530

    mobile view fix

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -131,7 +131,7 @@ export default function Contact() {
     <>
       <Toaster />
       <Navbar />
-      <section className="w-full py-12 md:py-24 lg:py-32">
+      <section className="w-full py-12 md:py-24 lg:py-32 mt-20">
         <div className="container px-4 md:px-6 space-y-8">
           <div className="space-y-4 text-center">
             <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-purple-600">
