8ab3d7ec3380280eeaa5eb14cceba60bcc5f6246
Romit  Dey <romit77dey@gmail.com>
2024-11-22 15:42:54 +0530

    removed recruitment modals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,19 @@ import { PoweredBy } from "@/components/component/powered-by";
 import WhyClub from "@/components/component/WhyClub";
 import { Footer } from "@/components/component/footer";
 import { lazy } from "react";
-import Overlay from "@/components/component/Overlays";
+// import Overlay from "@/components/component/Overlays";
 // import Hero from "@/components/Hero";
 // import Navbar from "@/components/component/Navbar";
 
-const Hero = lazy(()=>import("@/components/Hero")) ;
-const Navbar = lazy(()=>import("@/components/component/Navbar")) ;
+const Hero = lazy(() => import("@/components/Hero"));
+const Navbar = lazy(() => import("@/components/component/Navbar"));
 
 export default function page() {
   return (
     <div>
-      <Overlay/>
+      {/* <Overlay/> */}
       <Navbar />
-      <Hero/>
+      <Hero />
       <PoweredBy />
       <WhyClub />
       <Footer />
